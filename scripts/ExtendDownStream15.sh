50e2ed3a15980b39af4aaee5c542b8e65460b445
igortru <itolstoy@gmail.com>
2024-07-06 23:21:19 -0400

    genetic code 15

---
+++ scripts/ExtendDownStream15.sh
@@ -0,0 +1,18 @@
+script_name=$0
+script_full_path=$(dirname "$0")
+temp_dir=$(mktemp -d)
+
+L=$4
+if [ -z "$L" ]
+then
+       L="2" 
+fi
+$script_full_path/GenerateList.sh $1 $2 0 $L >$temp_dir/tmp
+P=$(cat $temp_dir/tmp | grep "*" | awk '{if ($2 != "TAG") { printf "%d\n",$1; exit;} }')
+if [ -z "$P" ] 
+then
+        echo "non-TAG stop codon missing\nadd downstream length" 
+	P=$(cat $temp_dir/tmp | wc -l)
+fi
+$script_full_path/GenerateDown.sh $temp_dir/tmp $P $3
+rm -rf $temp_dir
