12df0a9d52f2ec71085b8faeeb14e83ae2d902cb
igortru <itolstoy@gmail.com>
2024-07-13 23:54:18 -0400

    start end fixed

--- scripts/CELS.awk
+++ scripts/CELS.awk
@@ -2,13 +2,11 @@
 	if ($4 == "+") 
         {
 		X = $2-SUB*3;
-                while (X < 0) X = X + 3;
 		printf "%s\t%d\t%d\t %s\t11\n",$1,X,$3+ADD*3,P; 
 	}
 	else 
 	{
 		Y = $2-ADD*3;
- 		while (Y < 0) Y = Y + 3;
 		printf "%s\t%d\t%d\t %s\t11\n",$1,$3+SUB*3,Y,P;
 	}
 }
--- scripts/IdScan.py
+++ scripts/IdScan.py
@@ -6,6 +6,21 @@ ssels = [d.rstrip() for d in sels]
 for s in ssels:
         arr = s.split("\t")
         loc = (arr[1], arr[2], "plus","+") if int(arr[1]) < int(arr[2]) else (arr[2], arr[1], "minus","-")
+        command = ["efetch","-id",arr[0],"-db","nucleotide","-format","docsum"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        f = result.stdout.split("\n")
+        slen = 0
+        for s in f:
+           if s.find("<Slen>") > 0:
+              slen = int(s.split(">")[1].split("<")[0])       
+        sfrom = int(loc[0])
+        sto = int(loc[1])
+        while sfrom <= 0:
+           sfrom = sfrom + 3
+        while sto > slen:
+           sto = sto - 3       
+        loc = (str(sfrom),str(sto),loc[2],loc[3])
+
         command = ["efetch",
                    "-id", arr[0] , "-db", "nucleotide", "-format", "fasta" ,"-seq_start",loc[0], "-seq_stop", loc[1], "-strand", loc[2]]
         result = subprocess.run(command, capture_output=True, text=True)
