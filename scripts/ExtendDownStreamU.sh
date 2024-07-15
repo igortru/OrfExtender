d74d9fe7705b7bbb6e4712efc843f31d342a9f2c
igortru <itolstoy@gmail.com>
2024-07-15 09:25:25 -0400

    added ExtendDownStreamU.sh

---
+++ scripts/ExtendDownStreamU.sh
@@ -0,0 +1,19 @@
+script_name=$0
+script_full_path=$(dirname "$0")
+temp_dir=$(mktemp -d)
+
+AA="U"
+L=$3
+if [ -z "$L" ]
+then
+       L="2" 
+fi
+$script_full_path/GenerateList.sh $1 $2 0 $L >$temp_dir/tmp
+P=$(cat $temp_dir/tmp | grep "*" | awk '{if ($2 != "TGA") { printf "%d\n",$1; exit;} }')
+if [ -z "$P" ] 
+then
+        echo "non-TGA stop codon missing\nadd downstream extension length" 
+	P=$(cat $temp_dir/tmp | wc -l)
+fi
+$script_full_path/GenerateDown.sh $temp_dir/tmp $P $AA
+rm -rf $temp_dir
--- scripts/ExtendUpStreamU.sh
+++ scripts/ExtendUpStreamU.sh
@@ -1,19 +1,27 @@
 script_name=$0
 script_full_path=$(dirname "$0")
 temp_dir=$(mktemp -d)
-
 AA="U"
 L=$3
 if [ -z "$L" ]
 then
-       L="2" 
+       L="2"
 fi
-$script_full_path/GenerateList.sh $1 $2 0 $L >$temp_dir/tmp
-P=$(cat $temp_dir/tmp | grep "*" | awk '{if ($2 != "TGA") { printf "%d\n",$1; exit;} }')
-if [ -z "$P" ] 
+
+$script_full_path/GenerateList.sh $1 0 $2 $L >$temp_dir/tmp
+R=$(cat $temp_dir/tmp | egrep "TAA|TAG" | wc -l)
+
+if [ "$R" -eq "0" ] 
 then
-        echo "non-TGA stop codon missing\nadd downstream length" 
-	P=$(cat $temp_dir/tmp | wc -l)
+        echo "no upstream stop codons"
+        P="1"
+else
+    P=$(cat $temp_dir/tmp | sed '$d' | egrep "TAA|TAG" | tail -1 | awk  '{ printf "%d\n",$1; }')
+    if [ -z "$P" ]
+    then
+        P=$2
+    fi
 fi
-$script_full_path/GenerateDown.sh $temp_dir/tmp $P $AA
+R=$(cat $temp_dir/tmp | tail -1 | awk  '{ printf "%d\n",$1; }')
+$script_full_path/GenerateUp.sh $temp_dir/tmp $P $R $AA 
 rm -rf $temp_dir
